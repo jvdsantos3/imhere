5e1ff76a75f0a9aa1fe17beb1006f2ee63e8ddcc
João <jv.dossantos012@gmail.com>
2023-02-05 14:59:26 -0300

    Projeto I'm here

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,40 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+
+import { Participant } from "../../components/Participant";
+
+import { styles } from './styles'
+
+export function Home(){
+    function handleParticipanteAdd() {
+        console.log('Cliquei');
+    }
+
+    return (
+        <View style={ styles.container }>
+            <Text style={ styles.eventName }>
+                Nome do evento
+            </Text>
+
+            <Text style={ styles.eventDate }>
+                Sexta, 4 de Novembro de 2022.
+            </Text>
+
+            <View style={ styles.form }>
+                <TextInput 
+                    style={ styles.input }
+                    placeholder="Nome do participante"
+                    placeholderTextColor="#6B6B6B"
+                />
+
+                <TouchableOpacity style={ styles.button } onPress={handleParticipanteAdd}>
+                    <Text style={ styles.buttonText }>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
+
+            <Participant />
+            <Participant />
+        </View>
+    )
+}
